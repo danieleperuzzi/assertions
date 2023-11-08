7ac20c5fb819961aa435cb598cb36aa13f67d2b7
Daniele Peruzzi <daniele.peruzzi88@gmail.com>
2023-11-08 23:44:14 +0100

    added javadoc to DeclarativeAssertion + minor fix

--- lib/src/main/java/com/danieleperuzzi/assertion/DeclarativeAssertion.java
+++ lib/src/main/java/com/danieleperuzzi/assertion/DeclarativeAssertion.java
@@ -19,27 +19,61 @@ package com.danieleperuzzi.assertion;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
+/**
+ * Simple class to perform some conditional test in declarative and easy readable mode.
+ *
+ * The result of the application of this class would be
+ *
+ * <pre>{@code
+ * test(apiResponse)
+ *      .when(response -> response.getStatus() == 200)
+ *      .then(response -> validateSuccessResponse(response));
+ * }</pre>
+ *
+ * @param <T>   the type of the object to be tested
+ */
 public class DeclarativeAssertion<T> {
 
     private T object;
-    private boolean status;
+    private Predicate<T> predicate;
 
     private DeclarativeAssertion(T object) {
         this.object = object;
     }
 
+    /**
+     * Used to create a new DeclarativeAssertion instance, the main purpose is to wrap object
+     * instantiation with a method with an explicit name
+     *
+     * @param object    the object to be tested
+     * @return          a new DeclarativeAssertion instance
+     * @param <T>       the type of the object to be tested
+     */
     public static <T> DeclarativeAssertion<T> test(T object) {
         return new DeclarativeAssertion<>(object);
     }
 
+    /**
+     * Store this predicate for later use
+     *
+     * @param p     the predicate that tests the object
+     * @return      current DeclarativeAssertion instance
+     */
     public DeclarativeAssertion<T> when(Predicate<T> p) {
-        status = p.test(object);
+        predicate = p;
 
         return this;
     }
 
+    /**
+     * Perform the test on the stored object only if the given
+     * predicate is successful
+     *
+     * @param c     a consumer that represents the assertions on
+     *              the stored object
+     */
     public void then(Consumer<T> c) {
-        if (status) {
+        if (predicate.test(object)) {
             c.accept(object);
         }
     }
